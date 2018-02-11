2dea1bb66531c8d7e7617e87bd5b677c76e04a45
drewb <18552508+drewb101@users.noreply.github.com>
2018-02-11 03:42:56 -0600

    Added some code to Leap Year exercise for fun.

--- hw_assignments/chapter6_assignment.py
+++ hw_assignments/chapter6_assignment.py
@@ -11,28 +11,45 @@
 #
 # Write a function is_leap that takes a year as a parameter and returns True if the year is a leap year, False
 #   otherwise.
+
 def is_leap(year):
-    def is_leap(year):
-        if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    return True
-                else:
-                    return False
-            else:
-                return True
-        else:
-            return False
+       if year % 4 == 0:
+           if year % 100 == 0:
+               if year % 400 == 0:
+                   return True
+               else:
+                   return False
+           else:
+               return True
+       else:
+           return False
+
+def msg(tf):
+    if tf == True:
+        return "is"
+    else:
+        return "is not"
+
+
+year = int(input("Please enter a year to find out if it is a leap year or not."))
+tf = is_leap(year)
+
+print("The year", year, msg(tf), "a leap year.")
+
+
+
+
+
 # Below is a set of tests so you can check if your code is correct.
 # Do not copy this part into Vocareum.
-from test import testEqual
+# from test import testEqual
 
-testEqual(is_leap(1944), True)
-testEqual(is_leap(2011), False)
-testEqual(is_leap(1986), False)
-testEqual(is_leap(1956), True)
-testEqual(is_leap(1957), False)
-testEqual(is_leap(1800), False)
-testEqual(is_leap(1900), False)
-testEqual(is_leap(1600), True)
-testEqual(is_leap(2056), True)
+# testEqual(is_leap(1944), True)
+# testEqual(is_leap(2011), False)
+# testEqual(is_leap(1986), False)
+# testEqual(is_leap(1956), True)
+# testEqual(is_leap(1957), False)
+# testEqual(is_leap(1800), False)
+# testEqual(is_leap(1900), False)
+# testEqual(is_leap(1600), True)
+# testEqual(is_leap(2056), True)
