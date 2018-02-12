eb42a7149827d167312828dbf5b0f9e75645cacd
drewb101 <18552508+drewb101@users.noreply.github.com>
2018-02-11 23:07:10 -0600

    Ch. 9 assignment completed

--- hw_assignments/chapter9_assignment.py
+++ hw_assignments/chapter9_assignment.py
@@ -1,1 +1,11 @@
 def analyze_text(text):
+    text = "".join([i for i in text if i.isalpha()])
+    ltrs = len(text)
+    ltr_e = text.lower().count('e')
+    prct = str((ltr_e / ltrs) * 100)
+
+    message = ("The text contains {} alphabetic characters, of which {} ({}%) are 'e'." "".format(ltrs, ltr_e, prct))
+    return message
+
+
+print(analyze_text("How are you doing today kind sirs & pretty ladies?"))
