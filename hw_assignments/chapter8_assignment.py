4bc323ba2c89df5633f2d30540a1793e57de46ef
drewb101 <18552508+drewb101@users.noreply.github.com>
2018-02-11 21:14:45 -0600

    ch 8 assgn

--- hw_assignments/chapter8_assignment.py
+++ hw_assignments/chapter8_assignment.py
@@ -1,6 +1,31 @@
-# Write a course_grader function that takes a list of test scores as its parameter. It will add up these test scores and calculate an average score.
-#  It should then return a message of "pass" or "fail" depending on these two conditions:
-
-# * If the average score is greater than or equal to 70 AND no single test score is below 50, then return a message of "pass".
-# * If the average score is lower than 70 OR at least one test score is below 50, then return a message of "fail".
 def course_grader(test_scores):
+    # Your code here
+    if avg_score(test_scores) >= 70:
+        for score in test_scores:
+            whille
+            return "pass"
+        else:
+            return "fail"
+
+
+def avg_score(test_scores):
+    sum = 0
+    n = 0
+    for score in test_scores:
+        sum = sum + score
+        n += 1
+        avg = sum / n
+
+    return avg
+
+
+def main():
+    print(course_grader([100, 75, 45]))  # "fail"
+    print(course_grader([100, 70, 85]))  # "pass"
+    print(course_grader([80, 60, 60]))  # "fail"
+    print(course_grader([80, 80, 90, 30, 80]))  # "fail"
+    print(course_grader([70, 70, 70, 70, 70]))  # "pass"
+
+
+if __name__ == "__main__":
+    main()
