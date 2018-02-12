56eb89aec9f17af90cdff6804dee82a0b48f915e
drewb101 <18552508+drewb101@users.noreply.github.com>
2018-02-12 02:25:27 -0600

    moved

--- exercises.py
+++
@@ -1,3 +0,0 @@
-for i in range(100, 0, -1):
-    print(i, "Bottles of beer on the wall,", i, "bottles of beer! Take one down, pass it around,", i, "bottles of beer"
-                                                                                                      "on the wall!")
