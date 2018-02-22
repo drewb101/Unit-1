5b05f22ec7f749221e4cbd5f4e1bab8b4c58ebfd
drewb <18552508+drewb101@users.noreply.github.com>
2018-02-22 15:54:52 -0600

    Initials Assignment

--- hw_assignments/initials.py
+++ hw_assignments/initials.py
@@ -1,4 +1,15 @@
+def get_initials(fullname):
+    name_list = fullname.split()
+    init = ""
+    for name in name_list:
+        init = init + name[0]
+    print(init)
 
 
+def main():
+    fullname = input("What is your full name?")
 
+    print(get_initials)
 
+if __name__ == '__main__':
+    main()
